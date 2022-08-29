9c1bd78bb133b68b3a870e0ba8a644d60aba1d71
unknown <jakob.benthin@gmail.com>
2022-08-30 00:59:50 +0200

    Changed hangman
    Changed hangman.py

--- hangman.py
+++ hangman.py
@@ -1,14 +1,30 @@
-from itertools import count
-import random
+import random, os
 from words import words
 
-print(words)
 
+
+def check_letter(word, input, tmp_word):
+    if(input in word):
+        print(word.find(input))
+        print(f'{input}, finns i {word}')
+    else:
+        print(f'{input}, finns inte i {word}')
+    
+
+
+
+
+
+
+
+print(words)
 word = random.choice(words)
+
+
 
 print(word)
 
-print(len(word))
+#print(len(word))
 blanks = []
 for i in  range(0, len(word)):
     blanks.append('_')
@@ -18,5 +34,8 @@ print(*blanks, sep = " ")
 
 print(blanks[3])
 
+tmp_word = ''
+
+check_letter(word, input())
 
  

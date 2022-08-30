d54c7d8cb21add916f1348cfd81939f686e8e448
jake <jakob.benthin@gmail.com>
2022-08-30 15:33:52 +0200

    Changes in hangman.py

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
-import random, os
+import random, os, string
 from words import words
 
 
@@ -11,31 +11,157 @@ def check_letter(word, input, tmp_word):
         print(f'{input}, finns inte i {word}')
     
 
+def cls():
+    os.system('clear')
+
+
+def print_wrong(wrong):
+    cls()
+    if wrong == 1:
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('                 ---')
+        print("                |   |")
+    elif wrong == 2:
+        print('')
+        print('               \  |')
+        print('                \ |')
+        print('                 \|')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                 ---')
+        print("                |   |")
+    elif wrong == 3:
+        print('____________________')
+        print('               \  |')
+        print('                \ |')
+        print('                 \|')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                 ---')
+        print("                |   |")
+    elif wrong == 4:
+        print('_____________________')
+        print(' |             \  |')
+        print(' |              \ |')
+        print('                 \|')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                 ---')
+        print("                |   |")
+    elif wrong == 5:
+        print('_____________________')
+        print(' |             \  |')
+        print(' |              \ |')
+        print(' 0               \|')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                 ---')
+        print("                |   |")
+    elif wrong == 6:
+        print('_____________________')
+        print(' |             \  |')
+        print(' |              \ |')
+        print(' 0               \|')
+        print(' |                |')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                 ---')
+        print("                |   |")
+    elif wrong == 7:
+        print('_____________________')
+        print(' |             \  |')
+        print(' |              \ |')
+        print(' 0               \|')
+        print('-|                |')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                 ---')
+        print("                |   |")
+    elif wrong == 8:
+        print('_____________________')
+        print(' |             \  |')
+        print(' |              \ |')
+        print(' 0               \|')
+        print('-|-               |')
+        print('                  |')
+        print('                  |')
+        print('                  |')
+        print('                 ---')
+        print("                |   |")
+    elif wrong == 9:
+        print('_____________________')
+        print(' |             \  |')
+        print(' |              \ |')
+        print(' 0               \|')
+        print('-|-               |')
+        print('/                 |')
+        print('                  |')
+        print('                  |')
+        print('                 ---')
+        print("                |   |")
+    elif wrong == 10:
+        print('_____________________')
+        print(' |             \  |')
+        print(' |              \ |')
+        print(' 0               \|')
+        print('-|-               |')
+        print('/ \               |')
+        print('                  |')
+        print('                  |')
+        print('                 ---')
+        print("                |   |")
+
+def get_random_word(words):
+    word = random.choice(words)
+    return word
+
+
+def hangman_game():
+    word = get_random_word(words)
+
+    word_letters = set(word) # letters in word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+
+
+    blanks = []
+    for i in  range(0, len(word)):
+        blanks.append('-')
+    print("\n\n\n\n\n\n\n")
+    print(*blanks)
+    
 
 
 
 
 
 
-print(words)
-word = random.choice(words)
-
-
-
-print(word)
 
 #print(len(word))
-blanks = []
-for i in  range(0, len(word)):
-    blanks.append('_')
-    
-print(*blanks, sep = " ")
 
-
-print(blanks[3])
 
 tmp_word = ''
 
-check_letter(word, input())
+#check_letter(word, input(), tmp_word)
 
+#print_wrong(8)
+hangman_game()
  
